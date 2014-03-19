35d7a6ccab147c3225434343f30c6de3bb692059
Bianca Dy <deiparineninabianca@yahoo.com>
2014-03-19 15:13:45 +0800

    Student

--- application/controllers/verifylogin.php
+++ application/controllers/verifylogin.php
@@ -55,12 +55,12 @@ class VerifyLogin extends CI_Controller
         //query the database
         $student = $this->sms_model->loginStudent($username, $password);        
         $parent = $this->parent_model->loginParent($username, $password); 
-        // $teacher = $this->teacher_model->loginTeacher($username, $password);
+        $teacher = $this->teacher_model->loginTeacher($username, $password);
         // $chairperson = $this->chairperson_model->loginChairperson($username, $password);
         // $dean = $this->dean_model->loginDean($username, $password);
-        // $sao = $this->sao_model->loginSao($username, $password); 
-        // $sdpc = $this->sdpc_model->loginSdpc($username, $password);
-        // $admin = $this->admin_model->loginAdmin($username, $password);       
+        $sao = $this->sao_model->loginSao($username, $password); 
+        $sdpc = $this->sdpc_model->loginSdpc($username, $password);
+        $admin = $this->admin_model->loginAdmin($username, $password);       
         
         if($student)
         {
@@ -74,14 +74,14 @@ class VerifyLogin extends CI_Controller
         {
             $this->session->set_userdata('logged_in', $teacher);
         }
-        else if($chairperson)
-        {
-            $this->session->set_userdata('logged_in', $chairperson);
-        }
-        else if($dean)
-        {
-            $this->session->set_userdata('logged_in', $dean);
-        }
+        // else if($chairperson)
+        // {
+        //     $this->session->set_userdata('logged_in', $chairperson);
+        // }
+        // else if($dean)
+        // {
+        //     $this->session->set_userdata('logged_in', $dean);
+        // }
         else if($sao)
         {
             $this->session->set_userdata('logged_in', $sao);
